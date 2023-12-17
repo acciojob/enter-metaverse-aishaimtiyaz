3ff0471b3c4f499d41c8aaa3675ec6088adca52d
aishaimtiyaz <144771688+aishaimtiyaz@users.noreply.github.com>
2023-12-17 22:38:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 //your JS code here. If required.
-const divE=document.getElementsById("root");
-	divE.innerHTML +=`<p>Enter the Metaverse</p>
-                     <button>Enter</button> `;
+
+
+function updateText(){
+	var statusParagraph = document.getElementById("status");
+	statusParagraph.textContent="";
+	// Create an h1 element
+	var resultHeading = document.createElement("h1");
+	// Set the text content of the h1 element
+	resultHeading.textContent = "Entered Metaverse";
+	statusParagraph.appendChild(resultHeading);
+}
+
